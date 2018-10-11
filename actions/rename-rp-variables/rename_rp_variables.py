04584c6d010e4deefc2b52da535f78a67d1b9a15
Lee Zeman <lzeman@uvic.ca>
2018-10-11 11:18:43 -0700

    Add an update to variable descriptions.

--- actions/rename-rp-variables/rename_rp_variables.py
+++ actions/rename-rp-variables/rename_rp_variables.py
@@ -2,8 +2,8 @@
 
 '''The return period files as received have variables named
 rp.20 or rp.5, no matter which type of 5-year or 10-year event they
-describe. This script renames the variables to rp20tasmax or
-rp5pr.
+describe. This script renames the variables to something like 
+rp20tasmax or rp5pr.
 
 This is a procedure complicated enough to require a dedicated script
 because (by design) the netCDF interface provides no way to rename a
@@ -18,10 +18,19 @@ This script works by creating a copy of the original file with the
 variable name changed in the working directory, then deleting the
 original file and replacing it with the copy.
 
-It also corrects a minor error: in some of the return period files, 
-_FillValue is an integer, even though the rp variable is a float.
-Most tools will tolerate this, but some won't, and it's technically
-wrong.'''
+This script also adds possibly-missing metadata to the target variable:
+1) rewrites the variable's long_name attribute to be more descriptive
+2) sets the variable standard_name to the same as the variable name
+3) ensures that _FillValue is a float - some of the files have an integer
+4) sets the cell_methods attribute
+
+This script assumes that:
+ * The variable is a float
+ * Named rp.[number of years]
+ * With a long_name attribute "Annual Maximum|Minimum Tasmax|Tasmin|Precipitation"
+
+It will not work correctly if those conditions are not met.
+'''
 
 import argparse
 from netCDF4 import Dataset
@@ -62,11 +71,11 @@ else:
 model_output = None
 attributes = source.variables[rpvar].ncattrs()
 if "standard_name" in attributes:
-    match = re.match(r'Annual Maximum (\w*)', source.variables[rpvar].standard_name)
+    match = re.match(r'Annual (?:Maximum|Minimum) (\w*)', source.variables[rpvar].standard_name)
     if match:
         model_output = match.group(1)    
 if not model_output and "long_name" in attributes:
-    match = re.match(r'Annual Maximum (\w*)', source.variables[rpvar].long_name)
+    match = re.match(r'Annual (?:Maximum|Minimum) (\w*)', source.variables[rpvar].long_name)
     if match:
         model_output = match.group(1)
         
@@ -75,8 +84,12 @@ if not model_output:
     source.close()
     sys.exit()
     
+if not model_output in ["Tasmax", "Tasmin", "Precipitation"]:
+    print("Error: unknown model output {}, can't format metadata".format(model_output))
+    source.close()
+    sys.exit()
+    
 model_output = model_output.lower()
-print("model output = {}".format(model_output))
 model_output = "pr" if model_output == "precipitation" else model_output
 
 #copy variables and attributes to temperary file:
@@ -96,15 +109,31 @@ for var in source.variables:
         dest.variables[var].setncatts(source.variables[var].__dict__)
         dest.variables[var][:] = source.variables[var][:]
     else:
+        #the active variable. Needs a new name and some new metadata attributes.
         newvar = "rp{}{}".format(period, model_output)
         print("    Copying {} as {}".format(var, newvar))
         fill = source.variables[var].getncattr("_FillValue")
         print("      Updating fill value for {}".format(newvar))
         dest.createVariable(newvar, source.variables[var].dtype, source.variables[var].dimensions, 
                             fill_value=fill)
+        #clarify the confusing descriptions these files have by default
+        #by default, they say Annual [Maximum|Minimum] [Tasmax|Tasmin|Precipitation]
+        old_name = source.variables[var].getncattr("long_name").split(" ")
+        cell_method = old_name[1].lower() #"maximum" or "minimum"
+        var_desc = {"Tasmax": "daily maximum temperature",
+                    "Tasmin": "daily minimum temperature",
+                    "Precipitation": "one day precipitation amount"}[old_name[2]]
+        long_name = "{}-year annual {} {}".format(period, cell_method, var_desc)
+        print("      Updating long_name for {}".format(newvar))
+        dest.variables[newvar].setncattr("long_name", long_name)
+        print("      Updating standard_name for {}".format(newvar))
+        dest.variables[newvar].setncattr("standard_name", newvar)
+        print("      Updating cell_methods for {}".format(newvar))
+        dest.variables[newvar].setncattr("cell_methods", "time: {}".format(cell_method))
         atts = source.variables[var].__dict__;
         for att in atts:
-            if att != "_FillValue": #Fill value was set earlier.
+            #copy all other variable metadata (units, etc)
+            if not att in ["_FillValue", "long_name", "standard_name", "cell_methods"]: 
                 dest.variables[newvar].setncattr(att, source.variables[var].getncattr(att))
         dest.variables[newvar][:] = source.variables[var][:]
         
