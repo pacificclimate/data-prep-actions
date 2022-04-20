abc809bf0ce2ca883bdad2868eb8c4a321ffca59
Johnathan Helfrich <helfy16@telus.net>
2022-04-20 14:14:33 -0700

    add mouths of watersheds script

---
+++ actions/mouths-of-watersheds/mouths-of-watersheds.py
@@ -0,0 +1,46 @@
+import argparse
+import csv
+from netCDF4 import Dataset
+from shapely.geometry import shape
+import geojson
+from mow.helpers import *
+from mow.geo_data_grid_2d.vic import VicDataGrid
+
+parser = argparse.ArgumentParser('Output a CSV file with the lon lat of the mouth of each watershed')
+parser.add_argument('nc', help='an RVIC-netCDF file')
+parser.add_argument('geojson', help='a geoserver output file describing the watersheds')
+parser.add_argument('csv', help='the csv file to output the results to')
+args = parser.parse_args()
+nc = Dataset(args.nc, "r")
+flow_direction = VicDataGrid.from_nc_dataset(nc, "flow_direction")
+grid = VIC_direction_matrix(flow_direction.lat_step, flow_direction.lon_step) 
+
+
+with open(args.geojson) as f:
+    with open(args.csv, 'w', encoding='UTF8') as x:
+        writer = csv.writer(x)
+        header = ['lon', 'lat', 'properties']
+        writer.writerow(header)
+        gj = geojson.load(f)
+        for i in gj['features']:
+            center = shape(i.geometry).centroid
+            try:
+                xy = flow_direction.lonlat_to_xy(center.coords[0])
+            except:
+                continue
+
+            while True:
+                cell_routing = flow_direction.values[xy]
+                if cell_routing.mask:
+                    break
+                neighbour = vec_add(xy, grid[int(cell_routing)])
+                if flow_direction.is_valid_index(neighbour):
+                    xy = neighbour
+                else:
+                    break
+            if not cell_routing.mask:
+                mouth = flow_direction.xy_to_lonlat(xy)
+                line = [mouth[0].compressed()[0], mouth[1].compressed()[0], i.properties]
+                writer.writerow(line)
+
+nc.close()
---
+++ actions/mouths-of-watersheds/mow/helpers.py
@@ -0,0 +1,48 @@
+import math
+import operator
+
+def VIC_direction_matrix(lat_step, lon_step):
+    """ Return a VIC direction matrix, which is a matrix indexed by the VIC
+    streamflow direction codes 0...9, with the value at index `i` indicating
+    the offsets from the data index in a streamflow file required to
+    step in that streamflow direction.
+
+    :param lat_step: Difference between two successive latitudes in streamflow
+        file. (Only) sign matters.
+    :param lon_step: Difference between two successive longitudes in streamflow
+        file. (Only) sign matters.
+    :return: tuple of offset pairs
+
+    The offsets must account for the sign of the step in the lat and lon
+    dimensions in the streamflow file.
+    For example, in a streamflow file with lat and lon both increasing with
+    increasing index, the offset to step northeast is [1, 1].
+
+    Note that argument order is (lat, lon), not (lon, lat).
+    """
+    base = (
+        (0, 0),  # filler - 0 is not used in the encoding
+        (1, 0),  # 1 = north
+        (1, 1),  # 2 = northeast
+        (0, 1),  # 3 = east
+        (-1, 1),  # 4 = southeast
+        (-1, 0),  # 5 = south
+        (-1, -1),  # 6 = southwest
+        (0, -1),  # 7 = west
+        (1, -1),  # 8 = northwest
+        (0, 0),  # 9 = outlet
+    )
+    lat_dir = int(math.copysign(1, lat_step))
+    lon_dir = int(math.copysign(1, lon_step))
+    return tuple(
+        (lat_dir * lat_base, lon_dir * lon_base) for lat_base, lon_base in base
+    )
+
+def vec_add(a, b):
+    """numpy-style addition for builtin tuples: (1,1)+(2,3) = (3,4)"""
+    return tuple(map(operator.add, a, b))
+
+def neighbours(cell):
+    """Return all neighbours of `cell`: all cells with an x or y offset
+    of +/-1"""
+    return (vec_add(cell, offset) for offset in neighbour_offsets)
